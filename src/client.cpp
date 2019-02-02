dc6cb057f1f4f5f146d4289278e53c70ec96aaed
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2019-02-02 17:42:46 +0100

    Minimal build for two executables

--- src/client.cpp
+++ src/client.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "I am the client!" << std::endl;
     return 0;
 }
---
+++ src/server.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "I am the server!" << std::endl;
+    return 0;
+}
